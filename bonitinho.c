179a278ce773b53d7e65d7d14d3e1b3309b35c97
tiagofbfernandes <tiagofbfernandes@gmail.com>
2014-09-29 16:11:06 +0100

    tudo na main caralho

--- bonitinho.c
+++ bonitinho.c
@@ -5,25 +5,19 @@
 #include <time.h>
 
 #define INDICE_ID_FILE 7
+//					  1234567890     1234567890     1234567890     1234567890     1234567890     1234567890     1234567890     1234567890     1234567890     1234567890
+char cadeia[10][12]={"aaaaaaaaaa\n","bbbbbbbbbb\n","cccccccccc\n","dddddddddd\n","eeeeeeeeee\n","ffffffffff\n","gggggggggg\n","hhhhhhhhhh\n","iiiiiiiiii\n","jjjjjjjjjj\n"};
 
-char cadeia[10][11]={"aaaaaaaaa\n","bbbbbbbbbb\n","cccccccccc\n","dddddddddd\n","eeeeeeeeee\n","ffffffffff\n","gggggggggg\n","hhhhhhhhhh\n","iiiiiiiiii\n","jjjjjjjjjj\n"};
+int main(){
 
+	srand(time(NULL));
 
-int escolher_ficheiro(){
+	//escritor();
 
-	char file[]="SO2014-0.txt";
-	int id_file = rand() % 5;
-	file[INDICE_ID_FILE]+=id_file;
-
-	return open(file, O_WRONLY | O_CREAT);
-
-}
-
-int escritor(){
 	int file;
 	int escolhida;
 
-	for(int i=0; i<100; i++){
+	for(int i=0; i<2; i++){
 
 		//file = escolher_ficheiro();
 
@@ -40,25 +34,13 @@ int escritor(){
 			if(file < 0)
 		        printf("erro1 \n");   
 		 
-		    if(write(file,cadeia[escolhida],11) != 11)
+		    if(write(file,cadeia[escolhida],12) != 12)
 		        printf("erro2 \n");
 		    
 		}
 
 		close(file);
 	}
-	return 0;
-}
-
-
-
-int main(){
-
-	srand(time(NULL));
-
-	escritor();
-
-
 	return 0;
 }
 
